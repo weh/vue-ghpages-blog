41ce5ff09883d72705fb7cd3f3bd4b875068e952
Patrik Wehrli <patrik.wehrli@gmail.com>
2017-10-24 23:55:23 +0200

    Change blog posts repository

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ module.exports = {
   /**
    * Output
    */
-  blogTitle: 'Unknow Me',
+  blogTitle: 'Yes Weh Can',
   favicon: './static/favicon.png',
 
   /**
    * API
    */
-  repo: 'viko16/writings', // Which repository you stored the posts
+  repo: 'weh/vue-blog-posts',
   path: 'posts',
   branch: 'master'
 }
